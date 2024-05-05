89e07e40c292ec5a3f7751d0b9bd9b39a71df963
francisco <franciscomaderanatear@gmail.com>
2024-05-05 11:56:21 -0400

    GameBoard and Player classes created

--- script.js
+++ script.js
@@ -1,65 +1,88 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const boardDiv = document.querySelector(".board");
+    
+    const emptyCell = "-";
+    const x = "X";
+    const o = "O";
+
     // Gameboard object
     function GameBoard() {
-        const emptyCell = "-";
         const board = Array(9).fill(emptyCell);
 
         const setLetter = (place, letter) => (board[place] === emptyCell) ? board[place] = letter : board[place] = board[place];
-        const clearBoard = () => { for (let cell of board) cell = emptyCell; }
-        const showBoard = () => {
-            console.log(` ${board[0]}|${board[1]}|${board[2]}`);
-            console.log("-------");
-            console.log(` ${board[3]}|${board[4]}|${board[5]}`);
-            console.log("-------")
-            console.log(` ${board[6]}|${board[7]}|${board[8]}`);
+        const clearBoard = () => { for (let cell of board) cell = emptyCell };
+
+        const updateBoard = () => {
+            const cells = boardDiv.children;
+            for (let i = 0; i < board.length; i++) {
+                cells[i].innerHTML = `${board[i]}`;
+            }
         };
 
-        return { board, setLetter, clearBoard, showBoard };
+        const checkWinner = () => {
+            let first, second, third;
+
+            // Check horizontal
+            for (let i = 0; i < board.length; i += 3) {
+                first = board[i];
+                second = board[i + 1];
+                third = board[i + 2];
+
+                if (first === emptyCell) continue;
+
+                if (first === second && second === third) {
+                    return first;
+                }
+            }
+
+            // Check vertical
+            for (let i = 0; i < board.length; i += 3) {
+                first = board[i];
+                second = board[i + 3];
+                third = board[i + 6];
+
+                if (first === emptyCell) continue;
+
+                if (first === second && second === third) {
+                    return first;
+                }
+            }
+
+            // Check diagonal
+            if (board[0] === board[4] && board[4] === board[8]) return board[0];
+            if (board[2] === board[4] && board[4] === board[6]) return board[2];
+
+            return emptyCell;
+        };
+
+        const checkGameOver = () => {
+            if (checkWinner() !== emptyCell) return true;
+
+            for (let i = 0; i < board.length; i++) {
+                if (board[i] !== emptyCell) return false;
+            }
+
+            return true;
+        };
+
+        return { board, setLetter, clearBoard, updateBoard, checkWinner, checkGameOver };
     };
 
     // Players object
     function Player(playerLetter) {
-        const playerAction = () => {
-            do {
-                var cell = prompt("Select a cell number to play: ");
-            } while (cell > 9 || cell < 0);
-            return cell - 1;
-        };
-
-        return { playerLetter, playerAction };
+        let playerName = playerLetter;
+        return { playerLetter, playerName };
     }
 
     // Game object
-    function Game(gameBoard, playerX, playerO) {
-        let jugar = true;
-        let isX = true;
-        let jugada;
-        let player;
+    function Game(gameBoard) {
+        const player1 = Player(x);
+        const player2 = Player(o);
+        const players = [player1, player2];
 
-        const startGame = () => {
-            while(jugar) {
-                gameBoard.showBoard();
 
-                if (isX) {
-                    player = playerX;
-                } else {
-                    player = playerO;
-                }
-                isX = !isX;
-
-                jugada = player.playerAction();
-                gameBoard.setLetter(jugada, player.playerLetter);
-            }
-        }
-
-        return { startGame };
     }
 
-    const gameBoard = GameBoard();
-
-    const playerX = Player("X");
-    const playerO = Player("O");
-
-    let game = Game(gameBoard, playerX, playerO);
-    game.startGame();
+    const gameBoard = GameBoard(boardDiv);
+    const game = Game(gameBoard);
 });
